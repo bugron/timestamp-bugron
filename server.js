1f1485e124b3ee91bc9ac887b955635e58f4bcef
Glitch (timestamp-bugron) <none>
2018-11-09 08:13:06 +0000

    🎓😿 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+const cors = require('cors');
+app.use(cors({
+  optionSuccessStatus: 200
+}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get('/api/hello', function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/timestamp/:date_string?', function (req, res) {
+  const { date_string } = req.params;
+  const unixTimestamp = Number(date_string);
+  const timestamp = isNaN(unixTimestamp) ? date_string : unixTimestamp;
+  const date = timestamp ? new Date(timestamp) : new Date();
+
+  if (date.toString() == 'Invalid Date') {
+    return res.json({
+      error: 'Invalid Date',
+    });
+  }
+
+  res.json({
+    unix: date.getTime(), 
+    utc: date.toUTCString(),
+  });
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
